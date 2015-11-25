374292032b2f66ef9c717ea29bc3a6ffc809eae6
lcfeng <f839903061@163.com>
2015-11-25 17:44:21 +0800

    modify ServerActivity layout

--- app/src/main/java/hy/zc/wfj/activity/ServerActivity.java
+++ app/src/main/java/hy/zc/wfj/activity/ServerActivity.java
@@ -1,15 +1,32 @@
 package hy.zc.wfj.activity;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import hy.zc.wfj.R;
 
-public class ServerActivity extends Activity {
+public class ServerActivity extends FrameActivity {
 
+    private ImageButton common_title_back_btn;
+    private TextView tv_title;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_server);
+        initializeComponent();
+    }
+
+    private void initializeComponent() {
+        common_title_back_btn = (ImageButton) findViewById(R.id.common_title_back_btn);
+        tv_title = (TextView) findViewById(R.id.common_title_txt);
+        tv_title.setText("服务与反馈");
+        common_title_back_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goBack();
+            }
+        });
     }
 }
