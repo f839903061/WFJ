103603a28191dc29b6e66c76003474e779fe3696
lcfeng <f839903061@163.com>
2015-12-07 10:42:58 +0800

    add pulltorefreshlibrary

--- app/src/main/java/hy/zc/wfj/activity/SearchActivity.java
+++ app/src/main/java/hy/zc/wfj/activity/SearchActivity.java
@@ -2,19 +2,39 @@ package hy.zc.wfj.activity;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.text.format.DateUtils;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.facebook.drawee.backends.pipeline.Fresco;
-import com.facebook.drawee.view.SimpleDraweeView;
+import com.handmark.pulltorefresh.library.PullToRefreshBase;
+import com.handmark.pulltorefresh.library.PullToRefreshListView;
+
+import java.util.Arrays;
+import java.util.LinkedList;
 
 import hy.zc.wfj.R;
 
 public class SearchActivity extends FrameActivity implements View.OnClickListener{
 
-    private LinearLayout home_search_button;
+    private LinearLayout lay_back_btn;
+    private AutoCompleteTextView et_search;
+    private LinkedList<String> mListItems;
+    private PullToRefreshListView mPullRefreshListView;
+    private ArrayAdapter<String> mAdapter;
+    private Button btn_search;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,9 +45,113 @@ public class SearchActivity extends FrameActivity implements View.OnClickListene
     }
 
     private void initializeComponent() {
-        home_search_button=(LinearLayout)findViewById(R.id.home_search_button);
-        home_search_button.setOnClickListener(this);
+        lay_back_btn =(LinearLayout)findViewById(R.id.home_search_button);
+        btn_search=(Button)findViewById(R.id.search_btn);
+        et_search=(AutoCompleteTextView)findViewById(R.id.homeActivity_autoComplete);
+        lay_back_btn.setOnClickListener(this);
+        btn_search.setOnClickListener(this);
+//        这里是设置弹出软件键盘确认键的文本是搜索或发送或完成等字样，同时做相应的处理
+        et_search.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                boolean result = false;
+                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                    showToast("wan cheng");
+                }
+
+                return result;
+            }
+        });
+//        实时监听输入，作用就是来随时变化自动补全数据
+        et_search.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                showLogi("before");
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                showLogi("....");
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                showLogi("after");
+            }
+        });
+
+        mPullRefreshListView = (PullToRefreshListView) findViewById(R.id.pull_refresh_list);
+
+        // Set a listener to be invoked when the list should be refreshed.
+        mPullRefreshListView.setOnRefreshListener(new PullToRefreshBase.OnRefreshListener<ListView>() {
+            @Override
+            public void onRefresh(PullToRefreshBase<ListView> refreshView) {
+                String label = DateUtils.formatDateTime(getApplicationContext(), System.currentTimeMillis(),
+                        DateUtils.FORMAT_SHOW_TIME | DateUtils.FORMAT_SHOW_DATE | DateUtils.FORMAT_ABBREV_ALL);
+
+                // Update the LastUpdatedLabel
+                refreshView.getLoadingLayoutProxy().setLastUpdatedLabel(label);
+
+                // Do work to refresh the list here.
+                new GetDataTask().execute();
+            }
+        });
+
+        // Add an end-of-list listener
+        mPullRefreshListView.setOnLastItemVisibleListener(new PullToRefreshBase.OnLastItemVisibleListener() {
+
+            @Override
+            public void onLastItemVisible() {
+               showToast("End of List!");
+            }
+        });
+
+        ListView actualListView = mPullRefreshListView.getRefreshableView();
+
+        // Need to use the Actual ListView when registering for Context Menu
+        registerForContextMenu(actualListView);
+
+        mListItems = new LinkedList<String>();
+        mListItems.addAll(Arrays.asList(mStrings));
+
+        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mListItems);
+
+
+        // You can also just use setListAdapter(mAdapter) or
+        // mPullRefreshListView.setAdapter(mAdapter)
+        actualListView.setAdapter(mAdapter);
     }
+
+    private class GetDataTask extends AsyncTask<Void, Void, String[]> {
+
+        @Override
+        protected String[] doInBackground(Void... params) {
+            // Simulates a background job.
+            try {
+                Thread.sleep(1000);//刷新时间为1秒
+            } catch (InterruptedException e) {
+            }
+            return mStrings;
+        }
+
+        @Override
+        protected void onPostExecute(String[] result) {
+            mListItems.addFirst("Added after refresh...");
+            mAdapter.notifyDataSetChanged();
+
+            // Call onRefreshComplete when the list has been refreshed.
+            mPullRefreshListView.onRefreshComplete();
+
+            super.onPostExecute(result);
+        }
+    }
+
+
+    private String[] mStrings = { "Abbaye de Belloc", "Abbaye du Mont des Cats", "Abertam", "Abondance", "Ackawi",
+            "Acorn", "Adelost", "Affidelice au Chablis", "Afuega'l Pitu", "Airag", "Airedale", "Aisy Cendre",
+            "Allgauer Emmentaler", "Abbaye de Belloc", "Abbaye du Mont des Cats", "Abertam", "Abondance", "Ackawi",
+            "Acorn", "Adelost", "Affidelice au Chablis", "Afuega'l Pitu", "Airag", "Airedale", "Aisy Cendre",
+            "Allgauer Emmentaler" };
 
     @Override
     public void onClick(View v) {
@@ -36,6 +160,9 @@ public class SearchActivity extends FrameActivity implements View.OnClickListene
                 Intent goBack = getIntent();
                 setResult(Activity.RESULT_OK,goBack);
                 finish();
+                break;
+            case R.id.search_btn:
+                showToast("sldjfslkfdjslfj");
                 break;
             default:
                 break;
