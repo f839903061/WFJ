76fc84c2495ac0ecb0160b28f679c3ddbd6312e7
lcfeng <f839903061@163.com>
2015-12-07 17:23:44 +0800

    give commodity activity add listview , just a listview , do noting

--- app/src/main/java/hy/zc/wfj/activity/CommodityDetailsActivity.java
+++ app/src/main/java/hy/zc/wfj/activity/CommodityDetailsActivity.java
@@ -1,18 +1,44 @@
 package hy.zc.wfj.activity;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.format.DateUtils;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
+import com.alibaba.fastjson.JSON;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.facebook.drawee.backends.pipeline.Fresco;
+import com.handmark.pulltorefresh.library.PullToRefreshBase;
+import com.handmark.pulltorefresh.library.PullToRefreshListView;
+import com.handmark.pulltorefresh.library.extras.SoundPullEventListener;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+import hy.zc.wfj.App;
 import hy.zc.wfj.R;
+import hy.zc.wfj.data.SearchObject;
 
 public class CommodityDetailsActivity extends FrameActivity implements View.OnClickListener {
 
+    public static final String IS_SEARCH_OK = "sSearchOk";
     private LinearLayout mBack;
     private RadioGroup mRadioGroup;
+
+    private LinkedList<String> mListItems;
+    private PullToRefreshListView mPullRefreshListView;
+    private ArrayAdapter<String> mAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +51,17 @@ public class CommodityDetailsActivity extends FrameActivity implements View.OnCl
     }
 
     private void initializeComponent() {
+
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+        if (bundle != null) {
+
+            String uri = bundle.getString("uri");
+
+            getDataFromUri(uri);
+        }
+
+
         mBack = (LinearLayout) findViewById(R.id.home_search_button);
         mRadioGroup = (RadioGroup) findViewById(R.id.commodity_radiogroup);
         mBack.setOnClickListener(this);
@@ -51,6 +88,112 @@ public class CommodityDetailsActivity extends FrameActivity implements View.OnCl
                 showToast("数据暂时没有加载。。。");
             }
         });
+
+
+        listviewLoadData();
+
+
+    }
+
+    /**
+     * 初始化listview列表，现在使用的是pulltorefrashlistview
+     */
+    private void listviewLoadData() {
+        mPullRefreshListView = (PullToRefreshListView) findViewById(R.id.pull_refresh_list);
+
+        // Set a listener to be invoked when the list should be refreshed.
+        mPullRefreshListView.setOnRefreshListener(new PullToRefreshBase.OnRefreshListener<ListView>() {
+            @Override
+            public void onRefresh(PullToRefreshBase<ListView> refreshView) {
+                String label = DateUtils.formatDateTime(getApplicationContext(), System.currentTimeMillis(),
+                        DateUtils.FORMAT_SHOW_TIME | DateUtils.FORMAT_SHOW_DATE | DateUtils.FORMAT_ABBREV_ALL);
+
+                // Update the LastUpdatedLabel
+                refreshView.getLoadingLayoutProxy().setLastUpdatedLabel(label);
+
+                // Do work to refresh the list here.
+                new GetDataTask().execute();
+            }
+        });
+
+        // Add an end-of-list listener
+        mPullRefreshListView.setOnLastItemVisibleListener(new PullToRefreshBase.OnLastItemVisibleListener() {
+
+            @Override
+            public void onLastItemVisible() {
+                Toast.makeText(CommodityDetailsActivity.this, "End of List!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        ListView actualListView = mPullRefreshListView.getRefreshableView();
+
+        // Need to use the Actual ListView when registering for Context Menu
+        registerForContextMenu(actualListView);
+
+        mListItems = new LinkedList<String>();
+        mListItems.addAll(Arrays.asList(mStrings));
+
+        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mListItems);
+
+
+        // You can also just use setListAdapter(mAdapter) or
+        // mPullRefreshListView.setAdapter(mAdapter)
+        actualListView.setAdapter(mAdapter);
+    }
+
+    private class GetDataTask extends AsyncTask<Void, Void, String[]> {
+
+        @Override
+        protected String[] doInBackground(Void... params) {
+            // Simulates a background job.
+            try {
+                Thread.sleep(4000);
+            } catch (InterruptedException e) {
+            }
+            return mStrings;
+        }
+
+        @Override
+        protected void onPostExecute(String[] result) {
+            mListItems.addFirst("Added after refresh...");
+            mAdapter.notifyDataSetChanged();
+
+            // Call onRefreshComplete when the list has been refreshed.
+            mPullRefreshListView.onRefreshComplete();
+
+            super.onPostExecute(result);
+        }
+    }
+    private String[] mStrings = { "Abbaye de Belloc", "Abbaye du Mont des Cats", "Abertam", "Abondance", "Ackawi",
+            "Acorn", "Adelost", "Affidelice au Chablis", "Afuega'l Pitu", "Airag", "Airedale", "Aisy Cendre",
+            "Allgauer Emmentaler", "Abbaye de Belloc", "Abbaye du Mont des Cats", "Abertam", "Abondance", "Ackawi",
+            "Acorn", "Adelost", "Affidelice au Chablis", "Afuega'l Pitu", "Airag", "Airedale", "Aisy Cendre",
+            "Allgauer Emmentaler" };
+    private void getDataFromUri(String uri) {
+        StringRequest stringRequest=new StringRequest(Request.Method.GET, uri, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                if (response.length()>50) {
+                    SearchObject searchObject = JSON.parseObject(response, SearchObject.class);
+                    List<SearchObject.DataEntity> data = searchObject.getData();
+                    //下面的代码是将数据记载到listview中去
+                }else {
+                    showLogw("没有搜索到对应的数据");
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                showLoge("连接失败！");
+            }
+        });
+        App.addRequest(stringRequest, IS_SEARCH_OK);
+    }
+
+    @Override
+    protected void onStop() {
+        App.cancelAllRequests(IS_SEARCH_OK);
+        super.onStop();
     }
 
     @Override
