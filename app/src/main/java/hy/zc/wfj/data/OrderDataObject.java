b3ce904ef405d6b43404bb921b071ea2bf9c0c74
lcfeng <f839903061@163.com>
2015-12-15 15:33:49 +0800

    add complete title

--- app/src/main/java/hy/zc/wfj/data/OrderDataObject.java
+++ app/src/main/java/hy/zc/wfj/data/OrderDataObject.java
@@ -9,11 +9,14 @@ import java.io.Serializable;
  */
 public class OrderDataObject implements Serializable {
 
+    private static final long serialVersionUID = 7580665513070075990L;
     public static String TITLE_ALL = "全部订单";
     public static String TITLE_PAY = "待付款";
     public static String TITLE_SIGN = "待收货";
     public static String TITLE_COMMENT = "待评价";
     public static String TITLE_AFTER_SALE = "返修/退换";
+
+    public static String TITLE_COMPLETE="完成";
 
     public static String TITLE_KEY= "title";
 
